c8323e1fec0528b299b228f2f1104d5de1c935fb
Michael Egorov <michwill@gmail.com>
2020-02-24 18:52:17 -0800

    Inifinite approval (or, rather, non-infinite) wasn't present, so no this option yet

--- deposit.js
+++ deposit.js
@@ -29,8 +29,7 @@ async function handle_add_liquidity() {
     var amounts = $("[id^=currency_]").toArray().map(x => $(x).val());
     for (let i = 0; i < N_COINS; i++)
         amounts[i] = BigInt(Math.floor(amounts[i] / c_rates[i])).toString(); // -> c-tokens
-    if ($('#inf-approval').prop('checked'))
-        await ensure_allowance();
+    await ensure_allowance();
     var deadline = Math.floor((new Date()).getTime() / 1000) + trade_timeout;
     await swap.methods.add_liquidity(amounts, deadline).send({
         'from': default_account,
